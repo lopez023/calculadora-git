3109ef93f8622d86ca6133f1344fad5d203b5964
Eber Lopz <eber.huinil012@gmail.com>
2025-02-27 17:45:30 -0600

    cambios en la rama ramaCompa

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function agregarALaPantalla(value) {
   function limpiarPantalla() {
     document.getElementById('pantalla').value = '';
   }
+
+  function limpiar(){
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = valorPantalla.subtring(0, valorPantalla.length -1);
+    document.getElementById('pantalla').value = resultado
+
+  }
