39cbdf8794b04f1a5eb03bfd9bcf2b0b99e9fb1c
eberhuinil <elopezh30@miumg.edu.gt>
2025-02-27 21:05:28 -0600

    otra forma de eliminar el ultimo caracter

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ function agregarALaPantalla(value) {
   }
 
   function limpiar(){
-    const valorPantalla = document.getElementById('pantalla').value;
-    const resultado = valorPantalla.subtring(0, valorPantalla.length -1);
-    document.getElementById('pantalla').value = resultado
-
+    const valorPantalla = document.getElementById('pantalla').value
+      document.getElementById('pantalla').value = valorPantalla.slice(0,-1)
   }
